b68fecd6c47535c299de696b224ddc36f335428f
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-05-10 22:09:27 -0600

    updates functions
    i moved the array that will contain the value of our cards inside the uitlities files, also changed some functions like handleclicks to accept arguments

--- src/memo-test.js
+++ src/memo-test.js
@@ -1,47 +1,34 @@
-let myCards = ["img/fig-1.jpg", "img/fig-2.jpg", "img/fig-3.jpg", "img/fig-4.jpg", "img/fig-5.jpg", "img/fig-6.jpg"];
-
-function shuffleArray(array) {
-  const shuffledArray = array.slice();
-
-  shuffledArray.forEach((element, index) => {
-    const randomIndex = Math.floor(Math.random() * (index + 1));
-    [shuffledArray[index], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[index]];
-  });
-  return shuffledArray;
-}
-
 document.querySelector("#start-button").onclick = () => {
   startGame();
 };
 
 function startGame() {
-  const duplicatedCards = myCards.concat(myCards);
-  createCards(shuffleArray(duplicatedCards));
+  const cards = shuffledSetOfCards(cardValues);
+  console.log(cards);
+  createCards(cards);
 }
 
 document.querySelector("#table").onclick = (event) => {
-  handleClicks();
+  const $clickedCard = event.target;
+  handleClicks($clickedCard);
 };
 
 function createCards(cards) {
   const $containers = document.querySelectorAll(".cards-container");
   $containers.forEach((container, index) => {
+    const cardValue = cards[index];
     const $card = document.createElement("img");
-    console.log(cards);
-    $card.src = cards[index];
+    $card.src = `img/fig-${cardValue}.jpg`;
     $card.className = "img-thumbnail cards";
 
     container.appendChild($card);
   });
 }
 
-function handleClicks() {
-  const $clickedCard = event.target;
-  console.log($clickedCard);
-
+function handleClicks(click) {
   const $cards = document.querySelectorAll("#table .cards-container");
   $cards.forEach((card) => {
-    if (card.contains($clickedCard)) {
+    if (card.contains(click)) {
       card.className = "col-3 gy-1 flipped";
     }
   });
