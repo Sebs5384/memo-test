2e4262c20c5299e7a044733510197624196eddb9
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-05-12 14:20:32 +0200

    updates flipped class
    this is being done in order for the flipped class to be applied to the img rather than the div

--- src/memo-test.js
+++ src/memo-test.js
@@ -2,34 +2,31 @@ document.querySelector("#start-button").onclick = () => {
   startGame();
 };
 
-function startGame() {
-  const cards = shuffledSetOfCards(cardValues);
-  console.log(cards);
-  createCards(cards);
-}
-
 document.querySelector("#table").onclick = (event) => {
   const $clickedCard = event.target;
   handleClicks($clickedCard);
 };
 
+function startGame() {
+  const cards = shuffledSetOfCards(cardValues);
+  createCards(cards);
+  setPointerEvents("#table", "auto");
+}
+
 function createCards(cards) {
   const $containers = document.querySelectorAll(".cards-container");
+
   $containers.forEach((container, index) => {
     const cardValue = cards[index];
     const $card = document.createElement("img");
     $card.src = `img/fig-${cardValue}.jpg`;
     $card.className = "img-thumbnail cards";
-
     container.appendChild($card);
   });
 }
 
-function handleClicks(click) {
-  const $cards = document.querySelectorAll("#table .cards-container");
-  $cards.forEach((card) => {
-    if (card.contains(click)) {
-      card.className = "col-3 gy-1 flipped";
-    }
-  });
+function handleClicks(click) {}
+
+function setPointerEvents(selector, value) {
+  document.querySelector(selector).style.pointerEvents = value;
 }
--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,6 @@
 let cardValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+let previousClick = null;
+const matchedCards = [];
 
 function shuffleArray(array) {
   const shuffledArray = array.slice();
